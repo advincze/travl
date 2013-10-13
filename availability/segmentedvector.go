f3321243a13e02d200cb66751bf1f7f2b5800250
adam <advincze@gmail.com>
2013-10-13 17:30:02 +0200

    moved bit segment to segmented vector

--- availability/segmentedvector.go
+++ availability/segmentedvector.go
@@ -2,8 +2,33 @@ package availability
 
 import (
 	"bytes"
+	"math/big"
 	"strconv"
 )
+
+type BitSegment struct {
+	big.Int
+	start int
+}
+
+func (bs *BitSegment) SetUnit(position int, value byte) {
+	bs.SetBit(&bs.Int, position, uint(value))
+}
+
+func NewBitSegment(start int) *BitSegment {
+	return &BitSegment{
+		start: start,
+	}
+}
+
+func (bs *BitSegment) String() string {
+	var buffer bytes.Buffer
+	for i := 0; i < bs.BitLen(); i++ {
+		buffer.WriteString(strconv.Itoa(int(bs.Bit(i))))
+	}
+
+	return buffer.String()
+}
 
 type SegmentedVector struct {
 	segmentLength int
